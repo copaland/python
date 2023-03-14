55838c4aebbf7840ad70dc3648f9e681aa486da1
Nicky Kim <copa@seoultech.ac.kr>
2023-03-14 11:40:53 +0900

    Create 1variable.py

---
+++ ch01/1variable.py
@@ -0,0 +1,15 @@
+myName = "Lang Kim" # 카멜
+my_name = "김랑기" # 스네이크
+MyName = "kiki" # 파스칼
+_my_name = "korea"
+MYNAME = "God is love"
+my2name = "12345"
+# 2myname = '9876'
+# my-name = "michle"
+# my name = "kiki"
+myStr = '123' # str
+myNum = 123 # int
+
+print(myStr, myNum)
+print(type(myStr))
+print(type(myNum))
